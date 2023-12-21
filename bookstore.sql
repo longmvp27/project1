5e4fa96f557dfe1d5321652acdf34271fed311b0
DI-huyentm <huyen.cnh70@gmail.com>
2023-12-21 10:26:43 +0700

    update database

--- bookstore.sql
+++ bookstore.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 20, 2023 at 09:32 AM
+-- Generation Time: Dec 21, 2023 at 04:25 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
 
@@ -43,15 +43,27 @@ CREATE TABLE `books` (
   `id` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
   `author` varchar(255) NOT NULL,
-  `price` int(11) NOT NULL
+  `price` int(11) NOT NULL,
+  `image` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `books`
 --
 
-INSERT INTO `books` (`id`, `title`, `author`, `price`) VALUES
-(1, 'NodeJS', 'BaoAnh', 20000);
+INSERT INTO `books` (`id`, `title`, `author`, `price`, `image`) VALUES
+(1, 'Empire Of The Senseless', 'Acker Kathy', 170000, 'https://m.media-amazon.com/images/I/51kS07+nDwL.jpg'),
+(2, 'In Memoriam To Identity', 'Acosta Oscar Zeta', 210000, 'https://m.media-amazon.com/images/I/71G-8a7RpKL._AC_UF1000,1000_QL80_.jpg'),
+(3, 'Art Attack: A Short Cultural History of the Avant-Garde', 'Richard Brautigan', 130000, 'https://upload.wikimedia.org/wikipedia/en/thumb/f/f9/HawklineMonster.JPG/220px-HawklineMonster.JPG'),
+(4, 'Gang Of Souls: A Generation Of Beat Poets', 'Davis Stephen', 100000, 'https://upload.wikimedia.org/wikipedia/en/a/a6/Cover_of_Richard_Brautigan%27s_June_30th%2C_June_30th.jpg'),
+(5, 'Women Of The Left Bank: Paris 1900-1940', 'Franck Dan', 100000, 'https://rareamericana.cdn.bibliopolis.com/pictures/3732070_1.jpg?v=1662664912'),
+(6, 'The Goncourt Brothers', 'Franklin Benjamin V ed.', 220000, 'https://upload.wikimedia.org/wikipedia/en/2/24/RommelDrivesOnDeepIntoEgypt.jpg'),
+(7, 'My Life and Loves in Greenwich Village', 'Frees Paul', 150000, 'https://upload.wikimedia.org/wikipedia/en/c/c7/SombreroFallout.jpg'),
+(8, 'My Sisters Hand In Mine', 'French Warren', 270000, 'https://1960sdaysofrage.files.wordpress.com/2017/06/brautigancover.jpeg'),
+(9, 'Two Serious Ladies', 'Fritz James', 100000, 'https://mpd-biblio-covers.imgix.net/9780312277109.jpg?w=300'),
+(10, 'Let It Come Down', 'Conners Peter', 180000, 'https://m.media-amazon.com/images/I/81OBVqzun9L._AC_UF894,1000_QL80_.jpg'),
+(11, 'Stories of Paul Bowles', 'Cook Bruce', 170000, 'https://m.media-amazon.com/images/I/61I7IH8sryL._AC_UF1000,1000_QL80_.jpg'),
+(12, 'Paris By Night', 'Coolidge Clark', 290000, 'https://pictures.abebooks.com/inventory/md/md31335414127.jpg');
 
 -- --------------------------------------------------------
 
@@ -107,6 +119,15 @@ CREATE TABLE `users` (
   `address` varchar(255) DEFAULT NULL,
   `phone` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `password`, `gmail`, `role`, `address`, `phone`) VALUES
+(1, 'Long', '12345678', 'long@gmail.com', 'user', NULL, NULL),
+(2, 'Admin', '12345678', 'admin@gmail.com', 'admin', NULL, NULL),
+(3, 'Huyen', '12345678', 'huyen@gmail.com', 'user', NULL, NULL);
 
 --
 -- Indexes for dumped tables
@@ -168,7 +189,7 @@ ALTER TABLE `bookgenres`
 -- AUTO_INCREMENT for table `books`
 --
 ALTER TABLE `books`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `genres`
@@ -192,7 +213,7 @@ ALTER TABLE `sales`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints for dumped tables
